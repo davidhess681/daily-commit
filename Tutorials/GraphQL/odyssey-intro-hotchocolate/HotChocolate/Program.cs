fcbaeeb61dd16b956cd946afa923dde921e81294
David Hess <david.hess681@gmail.com>
2025-03-14 08:57:17 -0400

    connected to real spotify api
    and learned that the featured playlists endpoint was removed! gonna have to work around that in the next update

--- Tutorials/GraphQL/odyssey-intro-hotchocolate/HotChocolate/Program.cs
+++ Tutorials/GraphQL/odyssey-intro-hotchocolate/HotChocolate/Program.cs
@@ -13,7 +13,19 @@ builder.Services.AddCors();
 builder.Services.AddLogging();
 builder.Services.AddHttpLogging(_ => { });
 
-builder.Services.AddHttpClient<SpotifyService>();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddClientCredentialsTokenManagement()
+    .AddClient("spotify.token", client =>
+    {
+        client.TokenEndpoint = "https://accounts.spotify.com/api/token";
+        client.ClientId = builder.Configuration["Spotify:ClientId"];
+        client.ClientSecret = builder.Configuration["Spotify:ClientSecret"];
+        client.Scope = "app-remote-control playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-library-read user-library-modify user-read-private user-read-email user-follow-read user-follow-modify user-top-read user-read-playback-position user-read-playback-state user-read-recently-played user-read-currently-playing user-modify-playback-state ugc-image-upload streaming";
+    });
+
+builder.Services.AddHttpClient<SpotifyService>()
+    .AddClientCredentialsTokenHandler("spotify.token");
 
 var app = builder.Build();
 
