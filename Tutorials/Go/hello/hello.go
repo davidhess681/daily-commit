418457b156829bf988eb80b117c93ecae7393938
David Hess <david.hess681@gmail.com>
2025-05-21 08:56:33 -0400

    error handling in go

--- Tutorials/Go/hello/hello.go
+++ Tutorials/Go/hello/hello.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	// "rsc.io/quote"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// fmt.Println(quote.Go())
-	message := greetings.Hello("David")
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
