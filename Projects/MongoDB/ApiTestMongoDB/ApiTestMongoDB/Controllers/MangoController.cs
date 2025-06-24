222da067b6f606ac98d79fbd057394cd2acac668
David Hess <david.hess681@gmail.com>
2025-06-24 08:32:07 -0400

    mango api with mongo db

---
+++ Projects/MongoDB/ApiTestMongoDB/ApiTestMongoDB/Controllers/MangoController.cs
@@ -0,0 +1,49 @@
+﻿using ApiTestMongoDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace ApiTestMongoDB.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MangoController : ControllerBase
+{
+    private readonly MongoClient _mongoClient;
+
+    public MangoController(MongoClient mongoClient)
+    {
+        _mongoClient = mongoClient;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ListMangos()
+    {
+        var db = _mongoClient.GetDatabase("my_mangolicious_grocery_store");
+        var collection = db.GetCollection<Mango>("mangos");
+
+        var mangos = await collection.AsQueryable().ToListAsync();
+        return Ok(mangos);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddMango(Mango mango)
+    {
+        mango.Id = Guid.NewGuid().ToString();
+
+        var db = _mongoClient.GetDatabase("my_mangolicious_grocery_store");
+        var collection = db.GetCollection<Mango>("mangos");
+
+        await collection.InsertOneAsync(mango);
+        return Ok();
+    }
+
+    [HttpPost("initialize")]
+    public async Task<IActionResult> Initialize()
+    {
+        var db = _mongoClient.GetDatabase("my_mangolicious_grocery_store");
+        await db.CreateCollectionAsync("mangos");
+
+        return Ok();
+    }
+}
---
+++ Projects/MongoDB/ApiTestMongoDB/ApiTestMongoDB/Models/Mango.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiTestMongoDB.Models;
+
+public class Mango
+{
+    public string Id { get; set; }
+
+    public int WeightGrams { get; set; }
+
+    public string FarmName { get; set; }
+
+    public DateTime SellBy { get; set; }
+
+    public bool Organic { get; set; }
+}
